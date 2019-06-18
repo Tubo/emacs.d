1df143d664bd5d8f1d7cf17368dd9fc8cb783495
Tubo Shi <shitubo@gmail.com>
2019-06-19 03:18:35 +1200

    Tweaked keybindings and dired mode

--- main.el
+++ main.el
@@ -51,7 +51,9 @@
 (setq inhibit-splash-screen t
       inhibit-startup-message t)
 (electric-pair-mode)
-
+;; New shells shall spawn in side windows
+(add-to-list 'display-buffer-alist
+             '("*eshell" (display-buffer-in-side-window) (side . bottom)))
 
 ;; Good packages to start off with
 ;; ===============================
@@ -74,13 +76,22 @@
 ;; Magit
 ;; =====
 (use-package magit
-  :ensure t)
+  :ensure t
+  :general
+  (
+   "C-x g" 'magit-status
+   "C-x M-g" 'magit-dispatch
+   )
+  )
 
 
 ;; Ace window
 ;; ==========
 (use-package ace-window
-  :ensure t)
+  :ensure t
+  :general
+  ("C-x o" 'ace-window)
+  )
 
 ;; Ivy / Swiper / Counsel
 ;; ======================
@@ -91,13 +102,28 @@
   (setq enable-recursive-minibuffers t)
   :config
   (ivy-mode t)
+  :general
+  (
+   "C-s" 'swiper
+   "C-c C-r" 'ivy-resume
+   )
   )
+
+
 (use-package counsel
   :ensure t
   :config
   (counsel-mode t)
+  :general
+  (minibuffer-local-map
+   "C-r" 'counsel-minibuffer-history
+   )
   )
-(use-package swiper :ensure t)
+(use-package swiper
+  :ensure t
+  :general
+  ("C-s" 'swiper)
+  )
 
 ;; Evil
 ;; ====
@@ -110,23 +136,162 @@
   (evil-mode t)
   )
 (use-package evil-collection
-  :disabled
   :after evil
   :ensure t
   :config
-  (evil-collection-init)
+  (evil-collection-init '(help helpful magit custom diff-mode doc-view ediff elisp-mode elisp-refs epa eshell eww ibuffer imenu-list info package-menu popup term which-key))
   )
 
 
 ;; Dired-related
 ;; =============
+(use-package dired
+  :general
+  ('normal dired-mode-map
+           "q" 'quit-window
+           "j" 'dired-next-line
+           "k" 'dired-previous-line
+           [mouse-2] 'dired-mouse-find-file-other-window
+           [follow-link] 'mouse-face
+           ;; Commands to mark or flag certain categories of files
+           "#" 'dired-flag-auto-save-files
+           "." 'dired-clean-directory
+           "~" 'dired-flag-backup-files
+           ;; Upper case keys (except !) for operating on the marked files
+           "A" 'dired-do-find-regexp
+           "C" 'dired-do-copy
+           "B" 'dired-do-byte-compile
+           "D" 'dired-do-delete
+           "H" 'dired-do-hardlink
+           "L" 'dired-do-load
+           "M" 'dired-do-chmod
+           "O" 'dired-do-chown
+           "P" 'dired-do-print
+           "Q" 'dired-do-find-regexp-and-replace
+           "R" 'dired-do-rename
+           "S" 'dired-do-symlink
+           "T" 'dired-do-touch
+           "X" 'dired-do-shell-command
+           "Z" 'dired-do-compress
+           "c" 'dired-do-compress-to
+           "!" 'dired-do-shell-command
+           "&" 'dired-do-async-shell-command
+           ;; Comparison commands
+           "=" 'dired-diff
+           ;; move to marked files
+           "M-{" 'dired-prev-marked-file
+           "M-}" 'dired-next-marked-file
+           "%" nil
+           "%u" 'dired-upcase
+           "%l" 'dired-downcase
+           "%d" 'dired-flag-files-regexp
+           "%g" 'dired-mark-files-containing-regexp
+           "%m" 'dired-mark-files-regexp
+           "%r" 'dired-do-rename-regexp
+           "%C" 'dired-do-copy-regexp
+           "%H" 'dired-do-hardlink-regexp
+           "%R" 'dired-do-rename-regexp
+           "%S" 'dired-do-symlink-regexp
+           "%&" 'dired-flag-garbage-files
+           ;; mark
+           "*" nil
+           "**" 'dired-mark-executables
+           "*/" 'dired-mark-directories
+           "*@" 'dired-mark-symlinks
+           "*%" 'dired-mark-files-regexp
+           "*(" 'dired-mark-sexp
+           "*." 'dired-mark-extension
+           "*O" 'dired-mark-omitted
+           "*c" 'dired-change-marks
+           "*s" 'dired-mark-subdir-files
+           "*m" 'dired-mark
+           "*u" 'dired-unmark
+           "*?" 'dired-unmark-all-files
+           "*!" 'dired-unmark-all-marks
+           "U" 'dired-unmark-all-marks
+           "* <delete>" 'dired-unmark-backward
+           "* C-n" 'dired-next-marked-file
+           "* C-p" 'dired-prev-marked-file
+           "*t" 'dired-toggle-marks
+           ;; Lower keys for commands not operating on all the marked files
+           "a" 'dired-find-alternate-file
+           "d" 'dired-flag-file-deletion
+           "C-m" 'dired-find-file
+           "gr" 'revert-buffer
+           "I" 'dired-maybe-insert-subdir
+           "J" 'dired-goto-file
+           "K" 'dired-do-kill-lines
+           "r" 'dired-do-redisplay
+           "m" 'dired-mark
+           "t" 'dired-toggle-marks
+           "u" 'dired-unmark            ; also "*u"
+           "W" 'browse-url-of-dired-file
+           "x" 'dired-do-flagged-delete
+           "Y" 'dired-copy-filename-as-kill
+           "+" 'dired-create-directory
+           ;; open
+           "RET" 'dired-find-file
+           "gO" 'dired-find-file-other-window
+           "go" 'dired-view-file
+           ;; sort and search
+           "o" 'dired-sort-toggle-or-edit
+           ;; moving
+           "gp" 'dired-prev-dirline
+           "gn" 'dired-next-dirline
+           "gu" 'dired-up-directory
+           "gd" 'dired-up-directory
+           ;; hiding
+           "(" 'dired-hide-details-mode
+           )
+  )
 (use-package dired-hacks-utils
   :ensure t)
 (use-package dired-open
   :ensure t)
+(use-package dired-subtree
+  :ensure t
+  :general
+  ('normal dired-mode-map 
+           "i" 'dired-subtree-toggle
+           "TAB" 'dired-subtree-cycle
+           )
+  )
+(use-package dired-filter
+  :ensure t
+  :general
+  ('normal dired-mode-map
+           "f" 'dired-filter-mode)
+  )
+(use-package dired-rainbow
+  :ensure t
+  :config
+  (progn
+    (dired-rainbow-define-chmod directory "#6cb2eb" "d.*")
+    (dired-rainbow-define html "#eb5286" ("css" "less" "sass" "scss" "htm" "html" "jhtm" "mht" "eml" "mustache" "xhtml"))
+    (dired-rainbow-define xml "#f2d024" ("xml" "xsd" "xsl" "xslt" "wsdl" "bib" "json" "msg" "pgn" "rss" "yaml" "yml" "rdata"))
+    (dired-rainbow-define document "#9561e2" ("docm" "doc" "docx" "odb" "odt" "pdb" "pdf" "ps" "rtf" "djvu" "epub" "odp" "ppt" "pptx"))
+    (dired-rainbow-define markdown "#ffed4a" ("org" "etx" "info" "markdown" "md" "mkd" "nfo" "pod" "rst" "tex" "textfile" "txt"))
+    (dired-rainbow-define database "#6574cd" ("xlsx" "xls" "csv" "accdb" "db" "mdb" "sqlite" "nc"))
+    (dired-rainbow-define media "#de751f" ("mp3" "mp4" "MP3" "MP4" "avi" "mpeg" "mpg" "flv" "ogg" "mov" "mid" "midi" "wav" "aiff" "flac"))
+    (dired-rainbow-define image "#f66d9b" ("tiff" "tif" "cdr" "gif" "ico" "jpeg" "jpg" "png" "psd" "eps" "svg"))
+    (dired-rainbow-define log "#c17d11" ("log"))
+    (dired-rainbow-define shell "#f6993f" ("awk" "bash" "bat" "sed" "sh" "zsh" "vim"))
+    (dired-rainbow-define interpreted "#38c172" ("py" "ipynb" "rb" "pl" "t" "msql" "mysql" "pgsql" "sql" "r" "clj" "cljs" "scala" "js"))
+    (dired-rainbow-define compiled "#4dc0b5" ("asm" "cl" "lisp" "el" "c" "h" "c++" "h++" "hpp" "hxx" "m" "cc" "cs" "cp" "cpp" "go" "f" "for" "ftn" "f90" "f95" "f03" "f08" "s" "rs" "hi" "hs" "pyc" ".java"))
+    (dired-rainbow-define executable "#8cc4ff" ("exe" "msi"))
+    (dired-rainbow-define compressed "#51d88a" ("7z" "zip" "bz2" "tgz" "txz" "gz" "xz" "z" "Z" "jar" "war" "ear" "rar" "sar" "xpi" "apk" "xz" "tar"))
+    (dired-rainbow-define packaged "#faad63" ("deb" "rpm" "apk" "jad" "jar" "cab" "pak" "pk3" "vdf" "vpk" "bsp"))
+    (dired-rainbow-define encrypted "#ffed4a" ("gpg" "pgp" "asc" "bfe" "enc" "signature" "sig" "p12" "pem"))
+    (dired-rainbow-define fonts "#6cb2eb" ("afm" "fon" "fnt" "pfb" "pfm" "ttf" "otf"))
+    (dired-rainbow-define partition "#e3342f" ("dmg" "iso" "bin" "nrg" "qcow" "toast" "vcd" "vmdk" "bak"))
+    (dired-rainbow-define vc "#0074d9" ("git" "gitignore" "gitattributes" "gitmodules"))
+    (dired-rainbow-define-chmod executable-unix "#38c172" "-.*x.*")
+    )) 
+
 
 ;; Ranger
 (use-package ranger
+  :disabled
   :ensure t
   :config
   (ranger-override-dired-mode t)
@@ -175,15 +340,25 @@
   (setq eyebrowse-new-workspace t)
   (setq eyebrowse-wrap-around t)
   :config
-  (eyebrowse-setup-evil-keys)
   (eyebrowse-mode t))
 
 
 ;; Helpful
 ;; =======
 (use-package helpful
-  :ensure t)
+  :ensure t
+  :general
+  (
+   "C-h f" 'helpful-callable
+   "C-h k" 'helpful-key
+   "C-h v" 'helpful-variable)
+  )
 
+;; Which-key
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
 
 ;; Load custom.el
 ;; ==============
@@ -211,34 +386,30 @@
 (general-def
   "C-x k" 'kill-this-buffer)
 
-;; * Prefix Keybindings
-;; :prefix can be used to prevent redundant specification of prefix keys
-;; again, variables are not necessary and likely not useful if you are only
-;; using a definer created with `general-create-definer' for the prefixes
-;; (defconst my-leader "SPC")
-;; (defconst my-local-leader "SPC m")
-
 (general-create-definer my-leader-def
-  ;; :prefix my-leader
   :prefix "SPC")
-
-(general-create-definer my-local-leader-def
-  ;; :prefix my-local-leader
-  :prefix "SPC m")
 
 ;; ** Global Keybindings
 (my-leader-def
   :keymaps 'normal
   "a" 'org-agenda
-  "b" 'counsel-switch-buffer
+  "b" 'ivy-switch-buffer
   "c" 'org-capture
-  "d" 'ranger
+  "d" 'counsel-dired
   "e" 'counsel-find-file
   "g" 'magit-status
+  "k" 'kill-this-buffer
+  "o" 'ace-window
+  "s" 'eshell
   "1" 'eyebrowse-switch-to-window-config-1
   "2" 'eyebrowse-switch-to-window-config-2
   "3" 'eyebrowse-switch-to-window-config-3
+  "/" 'swiper
  )
+
+
+(general-create-definer my-local-leader-def
+  :prefix "SPC m")
 
 ;; ** Mode Keybindings
 (my-local-leader-def
@@ -249,20 +420,6 @@
   "s" 'org-narrow-to-subtree
   )
 
-;; Magit keybindings
-(general-def
-  "C-x g" 'magit-status
-  "C-x M-g" 'magit-dispatch)
-
-;; Ivy keybindings
-(general-def
-  "C-s" 'swiper
-  "C-c C-r" 'ivy-resume
-  )
-(general-def
-  :keymaps 'minibuffer-local-map
-  "C-r" 'counsel-minibuffer-history
-  )
 
 ;; Org keybindings
 (general-def
@@ -277,9 +434,3 @@
   "C-c C-q" 'counsel-org-tag
   "M-i" 'counsel-imenu
   )
-
-;; Helpful keybindings
-(general-def
-  "C-h f" 'helpful-callable
-  "C-h k" 'helpful-key
-  "C-h v" 'helpful-variable)
