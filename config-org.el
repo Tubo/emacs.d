b488a522cffc6405d5a320031677ee082acfb1c3
Tubo Shi <shitubo@gmail.com>
2019-07-14 16:43:59 +1200

    Updated Readme instruction. Refactored org face config.

--- config-org.el
+++ config-org.el
@@ -14,6 +14,11 @@
   (org-clock-persist 'history)  
   (org-agenda-todo-list-sublevels nil)
   (org-tags-column -100)
+  :custom-face
+  (org-level-1 ((t (:foreground "#DFAF8F" :height 1.4))))
+  (org-level-2 ((t (:foreground "#BFEBBF" :height 1.2))))
+  (org-level-3 ((t (:foreground "#7CB8BB" :height 1.0))))
+  (org-tag ((t (:height 0.7))))
   )
 
 
@@ -48,59 +53,21 @@
   :custom
   (org-variable-pitch-fixed-font "Andale Mono")
   :custom-face
-  (org-level-1 ((t (:foreground "#DFAF8F" :height 1.4))))
-  (org-level-2 ((t (:foreground "#BFEBBF" :height 1.2))))
-  (org-level-3 ((t (:foreground "#7CB8BB" :height 1.0))))
-  (org-tag ((t (:height 0.7))))
-  (org-special-keyword ((t (:inherit org-meta-line))))
   (variable-pitch ((t (:height 1.2 :family "Avenir Next"))))
   )
 
 (use-package org
   :if (eq system-type 'gnu/linux)
+  :custom
+  (org-variable-pitch-fixed-font "Course Code Variable")
   :custom-face
-  (org-beamer-tag ((t (:box (:line-width -1 :color grey40)))))
-  (org-block ((t (:inherit shadow :height 0.9 :family "Source Code Variable"))))
-  (org-checkbox ((t (:background "#5F5F5F" :foreground "#FFFFEF" :box (:line-width 1 :style released-button) :family "Source Code Variable"))))
-  (org-code ((t (:inherit shadow :height 0.8 :family "Source Code Variable"))))
-  (org-date ((t (:foreground "#8CD0D3" :underline t :height 0.8 :family "Source Code Variable"))))
-  (org-document-info ((t (:foreground "gold"))))
-  (org-document-info-keyword ((t (:inherit shadow))))
-  (org-document-title ((t (:foreground "#8CD0D3" :height 1.4 :family "Noto Sans"))))
-  (org-indent ((t (:family "Source Code Pro" :inherit org-hide))))
-  (org-level-1 ((t (:foreground "#DFAF8F" :height 1.4 :family "ETBembo" :weight bold))))
-  (org-level-2 ((t (:foreground "#BFEBBF" :height 1.3 :family "ETBembo" :weight bold))))
-  (org-level-3 ((t (:foreground "#7CB8BB" :height 1.2 :family "ETBembo" :weight bold))))
-  (org-meta-line ((t (:inherit font-lock-comment-face :height 0.9 :family "Source Code Variable"))))
-  (org-priority ((t (:foreground "gold" :family "Source Code Variable"))))
-  (org-special-keyword ((t (:inherit org-meta-line))))
-  (org-table ((t (:foreground "#9FC59F" :family "Source Code Variable"))))
-  (org-tag ((t (:foreground "dark gray" :weight ultra-light :height 0.8 :family "Source Code Variable"))))
-  (org-todo ((t (:foreground "gold" :weight bold))))
-  (variable-pitch ((t (:family "ETBembo" :height 1.2))))
+  (variable-pitch ((t (:height 1.2 :family "Noto Sans"))))
   )
-
 
 (use-package org
   :if (eq system-type 'windows-nt)
+  :custom
+  (org-variable-pitch-fixed-font "Course Code Variable")
   :custom-face
-  (org-beamer-tag ((t (:box (:line-width -1 :color grey40)))))
-  (org-block ((t (:inherit shadow :height 0.9 :family "Source Code Variable"))))
-  (org-checkbox ((t (:background "#5F5F5F" :foreground "#FFFFEF" :box (:line-width 1 :style released-button) :family "Source Code Variable"))))
-  (org-code ((t (:inherit shadow :height 0.8 :family "Source Code Variable"))))
-  (org-date ((t (:foreground "#8CD0D3" :underline t :height 0.8 :family "Source Code Variable"))))
-  (org-document-info ((t (:foreground "gold"))))
-  (org-document-info-keyword ((t (:inherit shadow))))
-  (org-document-title ((t (:foreground "#8CD0D3" :height 1.4 :family "Arial"))))
-  (org-indent ((t (:family "Source Code Pro" :inherit org-hide))))
-  (org-level-1 ((t (:foreground "#DFAF8F" :height 1.4 :family "DejaVu Serif" :weight bold))))
-  (org-level-2 ((t (:foreground "#BFEBBF" :height 1.3 :family "DejaVu Serif" :weight bold))))
-  (org-level-3 ((t (:foreground "#7CB8BB" :height 1.2 :family "DejaVu Serif" :weight bold))))
-  (org-meta-line ((t (:inherit font-lock-comment-face :height 0.9 :family "Source Code Variable"))))
-  (org-priority ((t (:foreground "gold" :family "Source Code Variable"))))
-  (org-special-keyword ((t (:inherit org-meta-line))))
-  (org-table ((t (:foreground "#9FC59F" :family "Source Code Variable"))))
-  (org-tag ((t (:foreground "dark gray" :weight ultra-light :height 0.8 :family "Source Code Variable"))))
-  (org-todo ((t (:foreground "gold" :weight bold))))
-  (variable-pitch ((t (:family "DejaVu Sans"))))
+  (variable-pitch ((t (:height 1.2 :family "DejaVu Sans"))))
   )
